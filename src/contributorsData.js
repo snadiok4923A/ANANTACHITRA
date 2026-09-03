1f8db68bf70e14bbd5a1d963ae70096301c0ceb8
Sandipan Paul <107307131+snadiok4923A@users.noreply.github.com>
2026-09-03 19:59:07 +0530

    Correct typo in contributorsData.js
    Fix typo in 'instagram' field and adjust formatting.

--- src/contributorsData.js
+++ src/contributorsData.js
@@ -3,7 +3,11 @@ export const CONTRIBUTORS_DATA = [
     id: 1,
     name: "Sandipan Paul",
     instagram: "https://www.instagram.com/aax0st/?__pwa=1"
-    }
-    
+    },
+  { 
+    id : 2,
+      name : "Sayan",
+      instergram: ""
+}
     
 ];
